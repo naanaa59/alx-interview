73ce341d8357cf7997f98b9246fad98ebaca2943
oumaima <oumaimanaanaapro@gmail.com>
2024-06-16 15:49:10 +0100

    edge cases

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,6 +7,8 @@
 
 def minOperations(n):
     """ calculates the minimum number of operations"""
+    if n < 2:
+        return 0
     for f in range(2, int(n ** 0.5) + 1):
         if n % f == 0:
             return f + minOperations(n // f)

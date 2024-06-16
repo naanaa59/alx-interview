ebb26d428b5bbc517b9a051dbc6c9240f3c167aa
oumaima <oumaimanaanaapro@gmail.com>
2024-06-16 15:45:42 +0100

    0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This script calculates the minimum number of operations to achieve
+    a given number of characters using only “Copy All” and “Paste”
+    operations
+"""
+
+
+def minOperations(n):
+    """ calculates the minimum number of operations"""
+    for f in range(2, int(n ** 0.5) + 1):
+        if n % f == 0:
+            return f + minOperations(n // f)
+    return n

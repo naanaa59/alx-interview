c11aae675cc2ec9fb32562251e24a8efd71cbb67
oumaima <oumaimanaanaapro@gmail.com>
2024-06-20 19:22:13 +0100

    finally

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,7 @@
 import sys
 import os
 import re
-from signal import signal, SIGINT, pause
+from signal import signal, SIGINT
 
 
 lines_count = 0
@@ -13,12 +13,21 @@ total_size = 0
 status_dict = {code: 0 for code in status_codes}
 
 
-def print_metrics(size, dicti):
+def print_metrics():
     """ print method"""
-    print(f"File size: {size}")
-    for code, count in sorted(dicti.items()):
+    print(f"File size: {total_size}")
+    for code, count in sorted(status_dict.items()):
         if count > 0:
             print(f"{code}: {count}")
+
+
+def handler(signum, frame):
+    """ signal handler"""
+    print_metrics()
+    raise KeyboardInterrupt
+
+
+signal(SIGINT, handler)
 
 
 try:
@@ -34,8 +43,6 @@ try:
                 status_dict[status] += 1
                 total_size += int(file_size)
         if lines_count % 10 == 0:
-            print_metrics(total_size, status_dict)
+            print_metrics()
 except Exception as e:
     pass
-finally:
-    print_metrics(total_size, status_dict)

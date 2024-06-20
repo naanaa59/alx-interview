af08bfdedcf299bde70ea47a2e564177bcf01f75
oumaima <oumaimanaanaapro@gmail.com>
2024-06-20 21:06:43 +0100

    0.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,27 +3,29 @@
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 counter = 0
 
+
 def print_metrics():
     '''Prints the metrics'''
     print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
+    for key, value in sorted(status_codes.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
 
+
 try:
     for line in sys.stdin:
-        line_list = line.split()
-        if len(line_list) > 4:
+        parts = line.split()
+        if len(parts) > 4:
             try:
-                code = line_list[-2]
-                size = int(line_list[-1])
-                if code in cache:
-                    cache[code] += 1
+                code = parts[-2]
+                size = int(parts[-1])
+                if code in status_codes:
+                    status_codes[code] += 1
                 total_size += size
                 counter += 1
             except (IndexError, ValueError):

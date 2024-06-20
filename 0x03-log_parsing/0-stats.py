7f5fb6833eea935c892cb4bf14a059b6fb6c992e
oumaima <oumaimanaanaapro@gmail.com>
2024-06-20 20:59:15 +0100

    00000000000000000000

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,40 @@
 #!/usr/bin/python3
-"""reads stdin line by line and computes metrics"""
+'''A script that reads stdin line by line and computes metrics'''
 
 import sys
-import re
-from signal import signal, SIGINT
 
-
-lines_count = 0
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-status_dict = {code: 0 for code in status_codes}
-
+counter = 0
 
 def print_metrics():
-    """ print method"""
-    print(f"File size: {total_size}")
-    for code, count in sorted(status_dict.items()):
-        if count > 0:
-            print(f"{code}: {count}")
-
-
-def handler(signum, frame):
-    """ signal handler"""
-    print_metrics()
-    sys.exit(0)
-
-
-signal(SIGINT, handler)
-
+    '''Prints the metrics'''
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
 
 try:
     for line in sys.stdin:
-        try:
-            # print(line.strip())
-            parts = line.split()
-            status_code, file_size = int(parts[-2]), int(parts[-1])
-            # print(status_code, file_size)
-            # print("///")
-            if status_code in status_dict:
-                # print("///")
-                status_dict[status_code] += 1
-                total_size += file_size
-            lines_count += 1
-            if lines_count % 10 == 0:
-                print_metrics()
-        except (IndexError, ValueError):
-            continue
+        line_list = line.split()
+        if len(line_list) > 4:
+            try:
+                code = line_list[-2]
+                size = int(line_list[-1])
+                if code in cache:
+                    cache[code] += 1
+                total_size += size
+                counter += 1
+            except (IndexError, ValueError):
+                continue
+
+        if counter == 10:
+            print_metrics()
+            counter = 0
+
 except KeyboardInterrupt:
     print_metrics()
     sys.exit(0)
+
+print_metrics()

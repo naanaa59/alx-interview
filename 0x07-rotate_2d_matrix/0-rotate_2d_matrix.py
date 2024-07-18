53ea4101cd166dc8ccc1f7e920cb5192a291ca14
oumaima <oumaimanaanaapro@gmail.com>
2024-07-18 18:06:25 +0100

    0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script rotates a 2D matrix clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    '''rotates a 2d matrix 90° clockwise
+    Returns: Nothing '''
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
